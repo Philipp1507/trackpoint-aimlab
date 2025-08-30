02d8c7bb8bf6e4d99ed58170cb28b6d93e95ffa8
philipp1507 <philipptrutz@posteo.de>
2025-08-30 17:48:07 +0200

    Some styling

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import random
 
 pg.init()
 
-width, height = 800, 600
-bg = (255, 255, 255)
-circle_color = (255, 0, 50)
+WIDTH, HEIGHT = 800, 600
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Trackpoint Aimlab")
 
-correct = pg.mixer.Sound("correct.mp3")
-error = pg.mixer.Sound("error.mp3")
+
+# Styling
+BG_COLOR = (255, 255, 255)
+CIRCLE_COLOR = (255, 0, 50)
+TEXT_COLOR = (0, 0, 0)
+FONT = pg.font.SysFont("FreeSans", 20)
+
+# Sound file init
+correct_sound = pg.mixer.Sound("correct_sound.mp3")
+error_sound = pg.mixer.Sound("error_sound.mp3")
 
 clock = pg.time.Clock()
 
-font = pg.font.SysFont("FreeSans", 20)
-text_color = (0, 0, 0)
-
-screen = pg.display.set_mode((width, height))
-pg.display.set_caption("Trackpoint Aimlab")
-
-x = random.randint(20, width - 20)
-y = random.randint(20, height - 20)
+# Initial Circle Placement
+x = random.randint(20, WIDTH - 20)
+y = random.randint(20, HEIGHT - 20)
 
 running = True
 start_time = pg.time.get_ticks()
@@ -30,26 +33,29 @@ while running:
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
+
             pos = pg.mouse.get_pos()
+
             if abs(int(pos[0]) - x) <= 20:
                 if abs(int(pos[1]) - y) <= 20:
-                    pg.mixer.Sound.play(correct)
+                    
+                    pg.mixer.Sound.play(correct_sound)
 
                     time_used = (pg.time.get_ticks() - start_time) / 1000
                     start_time = pg.time.get_ticks()
 
-                    x = random.randint(20, width - 20)
-                    y = random.randint(20, height - 20)
+                    x = random.randint(20, WIDTH - 20)
+                    y = random.randint(20, HEIGHT - 20)
                     
                     continue
-            pg.mixer.Sound.play(error)
+            pg.mixer.Sound.play(error_sound)
 
 
 
-    screen.fill(bg)
-    pg.draw.circle(screen, circle_color, (x, y), 20)
+    screen.fill(BG_COLOR)
+    pg.draw.circle(screen, CIRCLE_COLOR, (x, y), 20)
 
-    timer = font.render(str(time_used), True, text_color)
+    timer = FONT.render(str(time_used), True, TEXT_COLOR)
     screen.blit(timer, (20, 20))
 
     pg.display.flip()
